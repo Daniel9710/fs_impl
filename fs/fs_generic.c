2ef8f7fa9ef1814654b28b85dd9c3dfa5c3c0ab5
DanielChoi <dlsgur9710@naver.com>
2020-02-01 21:26:36 +0900

    Daniel

--- fs/fs_generic.c
+++ fs/fs_generic.c
@@ -94,9 +94,9 @@ void *fs_init (struct fuse_conn_info *conn, struct fuse_config *cfg) {
 	//fs_mkdir("/", 0755);
 	spb.cur_bit->bitset[0] = 5;
 	spb.cur_bit->bitset[3] = 5;
-	bitmap_write(spb.cur_bit, spb.cur_bit_bn);
+	pwrite(spb.fp, (void *)spb.cur_bit,  PAGESIZE, (spb.cur_bit_bn + D_BITMAP_INIT_BN) * PAGESIZE);
 	spb.cur_bit->bitset[5] = 2;
-	bitmap_read(spb.cur_bit, spb.cur_bit_bn);
+	pread(spb.fp, (void *)spb.cur_bit,  PAGESIZE, (spb.cur_bit_bn + D_BITMAP_INIT_BN) * PAGESIZE);
     for(int i = 0; i < 64; i++){
           for(int j = 0; j < 4; j++) {
                 p = 0;
